9f8e3363bd78349ee78ff886635849b33573b917
Fedor <fejuto@gmail.com>
2014-03-15 17:56:46 +0100

    basic scene

---
+++ SkyRabbitsUnity/Assets/scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float jumpForce;
+	public float walkSpeed;
+
+	PlayerControls playerControls;
+	public Transform groundedPoint;
+
+	private bool movingLeft;
+	private bool movingRight;
+	// Use this for initialization
+	void Start () {
+		playerControls = GetComponent<PlayerControls> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(playerControls.buttonLeft) && !Input.GetKey(playerControls.buttonRight)) {
+			movingLeft = true;
+			movingRight = false;
+			transform.localScale = new Vector3(-1, 1, 1);
+			//GetComponent<Rigidbody2D> ().velocity = new Vector2 (-100, GetComponent<Rigidbody2D> ().velocity.y);
+		} else if (!Input.GetKey(playerControls.buttonLeft) && Input.GetKey(playerControls.buttonRight)) {
+			movingLeft = false;
+			movingRight = true;
+			transform.localScale = new Vector3(1, 1, 1);
+			//GetComponent<Rigidbody2D> ().velocity = new Vector2 (100, GetComponent<Rigidbody2D> ().velocity.y);
+		} else if(!Input.GetKey(playerControls.buttonLeft) && !Input.GetKey(playerControls.buttonRight)){
+			movingLeft = false;
+			movingRight = false;
+		}
+
+		if(Input.GetKey(playerControls.buttonLeft) && Input.GetKey(playerControls.buttonRight) && IsGrounded()) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpForce);
+		}
+
+		if (movingLeft) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-walkSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		} else if (movingRight) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (walkSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		} else {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+
+		Debug.Log (GetComponent<Rigidbody2D> ().velocity);
+
+	}
+
+	bool IsGrounded(){
+		return Physics2D.OverlapPoint (new Vector2 (groundedPoint.position.x, groundedPoint.position.y)) != null && GetComponent<Rigidbody2D>().velocity.y <= 0.01;
+	}
+
+	/*public void OnGUI() {
+		if (Event.current.type == EventType.KeyDown) {
+			KeyPressedEventHandler(Event.current.keyCode);
+		}
+	}
+	
+	private void KeyPressedEventHandler(KeyCode keyCode) {
+		print (keyCode.ToString());
+	}*/
+}
---
+++ SkyRabbitsUnity/Assets/scripts/PlayerControls.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public KeyCode buttonLeft;
+	public KeyCode buttonRight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
