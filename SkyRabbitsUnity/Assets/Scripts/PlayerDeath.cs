b58eff4ba286342481f2e70c12c021a663902ccd
M.P. Korstanje <rien.korstanje@gmail.com>
2014-03-16 22:07:44 +0100

    Dead count

--- SkyRabbitsUnity/Assets/Scripts/PlayerDeath.cs
+++ SkyRabbitsUnity/Assets/Scripts/PlayerDeath.cs
@@ -13,19 +13,87 @@ public class PlayerDeath : MonoBehaviour {
 
 	public AudioClip deathSound;
 
+	private float timeOfDeath = 0.0f;
+	public float deathTime = 3.0f; // seconds.
+
+	private int deadCount = 0;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+
+	private Rigidbody2D rigidBody;
+
 	// Update is called once per frame
 	void Update () {
 		
-		checkAlive();
+		if(!checkAlive()){
 
+			if(timeOfDeath == 0.0){
+				timeOfDeath = Time.time;
+
+				deadCount++;
+
+				spawnBoneFountain();
+				audio.PlayOneShot(deathSound);
+
+
+
+
+
+				gameObject.rigidbody2D.isKinematic = true;
+				gameObject.collider2D.enabled = false;
+				gameObject.renderer.sortingLayerID = 2;
+				GetComponent<Player>().enabled = false;
+
+				Vector3 cameraPosition = Camera.main.transform.position;
+				Vector3 playerPosition = transform.position;
+
+				transform.position = new Vector3(cameraPosition.x, cameraPosition.y, playerPosition.z);
+				transform.localScale = Vector3.one * 5.0f;
+
+				((GameObject)GameObject.Find("DeadCount")).renderer.enabled = true;
+				((GameObject)GameObject.Find("DeadCount")).GetComponent<TextMesh>().text = "" + deadCount;
+			}
+
+		}
+
+		if (timeOfDeath != 0.0) {
+
+			float timeDeath = Time.time - timeOfDeath; 
+
+			if (timeDeath > deathTime) {
+					timeOfDeath = 0.0f;
+
+					gameObject.rigidbody2D.isKinematic = false;
+					gameObject.collider2D.enabled = true;
+					gameObject.renderer.sortingLayerID = 4;
+					GetComponent<Player> ().enabled = true;
+					gameObject.rigidbody2D.WakeUp ();
+					transform.localScale = Vector3.one;
+
+					gameObject.renderer.material.color = new Color(1,1,1,1);
+					((GameObject)GameObject.Find("DeadCount")).renderer.material.color = new Color(1,1,1,1);
+
+					((GameObject)GameObject.Find("DeadCount")).renderer.enabled = false;
+
+
+
+					respawn ();
+			} else {
+
+				float alpha = Mathf.Lerp(1,0,timeDeath/deathTime);
+
+				gameObject.renderer.material.color = new Color(1,1,1,alpha);
+				((GameObject)GameObject.Find("DeadCount")).renderer.material.color = new Color(1,1,1,alpha);
+
+			}
+		}
 	}
 
-	private void checkAlive(){
+	private bool checkAlive(){
 		
 		Vector3 cameraPosition = Camera.main.transform.position;
 		Vector3 playerPosition = transform.position;
@@ -38,11 +106,10 @@ public class PlayerDeath : MonoBehaviour {
 
 		if (Mathf.Abs(delta.x) >  verticalLimit  ||
 		    Mathf.Abs(delta.y) > horizontalLimit) {
-			spawnBoneFountain();
-			audio.PlayOneShot(deathSound);
-			respawn();
+			return false;
 		} 
 
+		return true;
 	}
 	
 	private void respawn(){
