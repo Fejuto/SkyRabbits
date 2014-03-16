343c63bcf80b339962224762b98e293227e2faf8
M.P. Korstanje <rien.korstanje@gmail.com>
2014-03-16 13:10:30 +0100

    Created bone fountain death effect.

---
+++ SkyRabbitsUnity/Assets/FadeOut.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeOut : MonoBehaviour {
+
+	public float lifeTime;
+	public float fadeTime;
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
